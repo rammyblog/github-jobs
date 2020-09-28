c5d9c78f59507f35f3c4f4170366fb76b121cbce
Onasanya Tunde <Onasanyatunde67@gmail.com>
2020-09-29 00:50:09 +0100

    Added cards

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
 import useFetchJob from "./useFetchJob"
 import { Container } from "react-bootstrap"
+import Job from "./Job"
 function App() {
-  const { jobs, loading, error } = useFetchJob()
+  const [params, setParams] = useState({})
+  const [page, setPage] = useState(1)
+
+  const { jobs, loading, error } = useFetchJob(params, page)
   return (
-    <Container>
+    <Container className="my-4">
       {loading && <h1>Loading....</h1>}
       {error && <h1>Error, Try refreshing</h1>}
-      <h1>{jobs.length}</h1>
+      <>
+        <h1>GitHub Jons</h1>
+        {jobs.map((job) => {
+          return <Job key={job.id} job={job} />
+        })}
+      </>
     </Container>
   )
 }
